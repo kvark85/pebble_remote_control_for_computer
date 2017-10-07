d0214a5d797cb8754cab522532e1fc23db63c8f1
kvark85 <kvark1985@gmail.com>
2017-10-07 20:02:36 +0300

    Добавил web интерфейс.

--- back end (for computer)/backEndRemote.js
+++ back end (for computer)/backEndRemote.js
@@ -2,11 +2,16 @@ var express = require('express');
 var robot = require("robotjs");
 var app = express();
 
+app.use(express.static('public'));
+
 app.get('/:control/:command/', function (req, res) {
     var control = req.params.control,
         command = req.params.command;
     robot.keyTap(command);
-    res.json({comand: `This is ${control}, comand: ${command}`, answer: `ok`});
+
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.json({comand: `This is ${control}`, comand: `${command}`, answer: `ok`});
 });
 
 app.listen(3000, function () {
